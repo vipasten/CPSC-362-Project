5c36cfbce30a53a2f7a1d17e0cfcfe03877b9b06
vipasten <vipasten@csu.fullerton.edu>
2026-04-29 22:04:56 -0700

    couple changes

--- new files/Spring/src/main/java/com/example/demo/auth/UserRepository.java
+++ new files/Spring/src/main/java/com/example/demo/auth/UserRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.auth;
 
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UserRepository extends JpaRepository<UserAccount, Long> {
